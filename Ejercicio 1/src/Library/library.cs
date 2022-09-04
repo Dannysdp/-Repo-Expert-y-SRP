e2e6919c8def710486225835720b4139778aa505
[Pedro Scheeffer] <[pedrosd26@gmail.com]>
2022-09-04 15:33:59 -0300

    Cambios en library

--- Ejercicio 1/src/Library/library.cs
+++ Ejercicio 1/src/Library/library.cs
@@ -8,6 +8,11 @@ namespace SRP
         List<Book> libraryList = new List<Book>();
         public library(){
         }
+        public void ShelveBook(Book book){
+            if(!libraryList.Contains(book)){
+                libraryList.Add(book);
+            }
+        }
         public void ShelveBook(String sector, String shelve,Book book)
         {
             book.LibrarySector = sector;
